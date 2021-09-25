ef7c2ecf5349a8e4b67764ccb6c1d4fdf3ee518c
Luis Carlos <luis.carlos@jornadatecnologia.com>
2021-09-24 23:02:52 -0300

    [features/Pages] Gerando páginas estáticas.

--- pages/cursos/[cursoId].js
+++ pages/cursos/[cursoId].js
@@ -1,5 +1,23 @@
-export async function getServerSideProps(context) {
-  const id = context.query.cursoId;
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: {
+          cursoId: "1",
+        },
+      },
+      {
+        params: {
+          cursoId: "2",
+        },
+      },
+    ],
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const id = context.params.cursoId;
   const response = await fetch("http://localhost:3000/api/cursos/" + id);
   const data = await response.json();
   return {
